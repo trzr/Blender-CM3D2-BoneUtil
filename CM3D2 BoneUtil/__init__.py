6fc48e8296f5aa4da0bc40c1c6d37c7bd3d255a6
trzr <trzr@users.noreply.github.com>
2016-12-24 15:48:06 +0900

    バージョン更新 0.2.9

--- CM3D2 BoneUtil/__init__.py
+++ CM3D2 BoneUtil/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
 	"name" : "CM3D2 BoneUtil",
 	"author" : "trzr",
-	"version" : (0, 2, 8),
+	"version" : (0, 2, 9),
 	"blender" : (2, 76, 0),
 	"location" : "AddonDesc",
 	"description" : "",
